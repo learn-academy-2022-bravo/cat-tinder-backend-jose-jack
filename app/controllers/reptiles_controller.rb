cf1004145983059ae068b1cd587e36c758023493
Noeyeddeer <john.e.mades@gmail.com>
2022-05-12 14:41:28 -0700

    API-Endpoints 8/8 Completed

--- app/controllers/reptiles_controller.rb
+++ app/controllers/reptiles_controller.rb
@@ -1,2 +1,32 @@
 class ReptilesController < ApplicationController
+    
+    def index
+        reptiles = Reptile.all
+        render json: reptiles
+    end
+  
+    def create
+        reptile = Reptile.create(reptile_params)
+        render json: reptile
+    end
+  
+    def update
+        reptile = Reptile.find(params[:id])
+        reptile.update(reptile_params)
+        render json: reptile
+    end
+  
+    def destroy
+        reptile = Reptile.find(params[:id])
+        if reptile.destroy
+            render json: reptile
+        else
+            render json: reptile.errors
+        end
+    end
+
+    private
+    def reptile_params
+        params.require(:reptile).permit(:name, :age, :enjoys, :image)
+    end
 end
--- spec/requests/reptiles_spec.rb
+++ spec/requests/reptiles_spec.rb
@@ -2,6 +2,87 @@ require 'rails_helper'
 
 RSpec.describe "Reptiles", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of reptiles" do
+      Reptile.create(
+        name: 'Fred',
+        age: 5,
+        enjoys: 'Eating bugs',
+        image: 'https://i.natgeofe.com/k/c02b35d2-bfd7-4ed9-aad4-8e25627cd481/komodo-dragon-head-on_16x9.jpg?w=1200'
+      )
+
+      get '/reptiles'
+
+      reptile = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(reptile.length).to eq 1
+    end
+  end
+
+  describe "POST /create" do
+    it "creates a reptile" do
+      reptile_params = {
+        reptile: {
+          name: 'Fred',
+          age: 5,
+          enjoys: 'Eating bugs',
+          image: 'https://i.natgeofe.com/k/c02b35d2-bfd7-4ed9-aad4-8e25627cd481/komodo-dragon-head-on_16x9.jpg?w=1200'
+        }
+      }
+
+      post '/reptiles', params: reptile_params
+
+      expect(response).to have_http_status(200)
+      reptile = Reptile.first
+      expect(reptile.name).to eq 'Fred'
+    end
+  end
+
+  describe "PATCH /update" do
+    it 'can update an existing reptile' do
+      reptile_params = {
+        reptile: {
+          name: 'Fred',
+          age: 5,
+          enjoys: 'Eating bugs',
+          image: 'https://i.natgeofe.com/k/c02b35d2-bfd7-4ed9-aad4-8e25627cd481/komodo-dragon-head-on_16x9.jpg?w=1200'
+        }
+      }
+
+      post '/reptiles', params: reptile_params
+      reptile = Reptile.first
+
+      update_reptile_parmas = {
+        reptile: {
+          name: 'Fred',
+          age: 5,
+          enjoys: 'Eating bugs',
+          image: 'https://i.natgeofe.com/k/c02b35d2-bfd7-4ed9-aad4-8e25627cd481/komodo-dragon-head-on_16x9.jpg?w=1200'
+        }
+      }
+      patch "/reptiles/#{reptile.id}", params: update_reptile_parmas
+      updated_reptile = Reptile.find(reptile.id)
+      expect(response).to have_http_status(200)
+      expect(updated_reptile.age).to eq(5)
+    end
+  end
+
+  describe "DELETE /destroy" do
+    it 'can delete an existing reptile' do
+      reptile_params = {
+        reptile: {
+          name: 'Fred',
+          age: 5,
+          enjoys: 'Eating bugs',
+          image: 'https://i.natgeofe.com/k/c02b35d2-bfd7-4ed9-aad4-8e25627cd481/komodo-dragon-head-on_16x9.jpg?w=1200'
+        }
+      }
+
+      post '/reptiles', params: reptile_params
+
+      expect(response).to have_http_status(200)
+      reptile = Reptile.first
+      expect(reptile.name).to eq 'Fred'
+    end
   end
 end
+
